490697c2c32d11b60b562b3009d001c9697afd45
Christian Oneal <legendhosting@live.com>
2016-01-19 13:38:23 -0500

    Update to the API test.

--- test/js/api.js
+++ test/js/api.js
@@ -2,10 +2,61 @@
    console.log("Twitch API Loaded");
   });
 Twitch.api({method: 'channels/monstercat'},  function(error, channel) {
-  document.getElementById('url').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.name + '</a>';
+  document.getElementById('url').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.display_name + '</a>';
   document.getElementById('status').innerHTML += channel.status
 });
 Twitch.api({method: 'channels/hui101'},  function(error, channel) {
-  document.getElementById('url2').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.name + '</a>';
+  document.getElementById('url2').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.display_name + '</a>';
   document.getElementById('status2').innerHTML += channel.status
 });
+Twitch.api({method: 'channels/heyitspassion'},  function(error, channel) {
+  document.getElementById('url3').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.display_name + '</a>';
+  document.getElementById('status3').innerHTML += channel.status
+});
+Twitch.api({method: 'channels/jaysc2'},  function(error, channel) {
+  document.getElementById('url4').innerHTML += '<a href="' + channel.url + '" target="_blank" >' + channel.display_name + '</a>';
+  document.getElementById('status4').innerHTML += channel.status
+});
+
+
+// Offline and Online System
+
+Twitch.api({method: 'streams/monstercat'},  function(error, stream) {
+	  if (stream.stream === null) {
+	  	document.getElementById('onoff').innerHTML += '<i class="fa fa-ban fa-2x"></i>'
+} else {
+    document.getElementById('onoff').innerHTML += '<i class="fa fa-check-circle-o fa-2x"></i>'
+}
+});
+Twitch.api({method: 'streams/hui101'},  function(error, stream) {
+	  if (stream.stream === null) {
+	  	document.getElementById('onoff2').innerHTML += '<i class="fa fa-ban fa-2x"></i>'
+} else {
+    document.getElementById('onoff2').innerHTML += '<i class="fa fa-check-circle-o fa-2x"></i>'
+}
+});
+Twitch.api({method: 'streams/heyitspassion'},  function(error, stream) {
+	  if (stream.stream === null) {
+	  	document.getElementById('onoff3').innerHTML += '<i class="fa fa-ban fa-2x"></i>'
+} else {
+    document.getElementById('onoff3').innerHTML += '<i class="fa fa-check-circle-o fa-2x"></i>'
+}
+});
+Twitch.api({method: 'streams/jaysc2'},  function(error, stream) {
+	  if (stream.stream === null) {
+	  	document.getElementById('onoff4').innerHTML += '<i class="fa fa-ban fa-2x"></i>'
+} else {
+    document.getElementById('onoff4').innerHTML += '<i class="fa fa-check-circle-o fa-2x"></i>'
+}
+});
+
+
+
+
+
+
+
+
+
+
+
