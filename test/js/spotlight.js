f2f5efe7a66faa14e788ae539913925c5ae703b7
Christian Oneal <legendhosting@live.com>
2016-01-21 12:02:27 -0500

    Spotlight System

--- test/js/spotlight.js
+++ test/js/spotlight.js
@@ -4,7 +4,7 @@ var member = "Monstercat"
 
 if (spotlight === 1) {
 	 document.getElementById('spotlight').innerHTML += '<h3 class="hvr-hang">' + member + ' </h3>';
-	 document.getElementById('spotlight_window').innerHTML += '<iframe src="http://player.twitch.tv/?channel=Monstercat&autoplay=false" frameborder="0" scrolling="no" height="260" width="460"></iframe>';
+	 document.getElementById('spotlight_window').innerHTML += '<iframe src="http://player.twitch.tv/?channel=' + member + '&autoplay=false" frameborder="0" scrolling="no" height="310" width="510"></iframe>';
 } else {
 	document.getElementById('spotlight').innerHTML += '<h3 class="hvr-hang">No one atm :(</h3>';
 }
